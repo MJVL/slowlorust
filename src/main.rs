e830000b9c6254e5d74b7009e1963852f7124abe
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2021-11-02 16:33:08 -0400

    Add threadpool

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use rand::Rng;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::time::Duration;
-use std::{thread, time};
+use std::thread;
+use scoped_threadpool::Pool;
 
 fn infinite_worker(num: u8, sleep_time: u8) {
     println!("[slowloris_{}] Spawned.", num);
@@ -28,10 +29,15 @@ fn infinite_worker(num: u8, sleep_time: u8) {
 }
 
 fn main() {
-    const NUM_THREADS: u8 = 5;
+    const NUM_THREADS: u8 = 15;
     const SECONDS: u8 = 1;
 
-    for i in 0..NUM_THREADS {
-        infinite_worker(i, SECONDS);
-    }
+    let mut pool = Pool::new(NUM_THREADS as u32);
+    pool.scoped(|scoped| {
+        for i in 0..NUM_THREADS {
+            scoped.execute(move || {
+                infinite_worker(i, SECONDS);
+            });
+        }
+    });
 }
